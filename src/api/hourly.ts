6d31ad0093d9361350bdc2f5fafae87eb7a49b08
Stephen <stephenn649@gmail.com>
2025-05-06 10:28:24 -0500

    Sets up some of the api calls in api/hourly.ts

--- src/api/hourly.ts
+++ src/api/hourly.ts
@@ -13,4 +13,34 @@ export const getTopHourlyDepts = async (url: string, date: string, hour: string)
     }
   });
   return json;
-}
+};
+
+export const getTopHourlySubDepts = async (url: string, date: string, hour: string) => {
+  const json = await axios({
+    method: "GET",
+    header: {
+      "Content-Type": "application/json",
+    },
+    url: url + "hourlysales/subdepts",
+    params: {
+      date,
+      hour
+    }
+  });
+  return json;
+};
+
+export const getTopHourlyCats = async (url: string, date: string, hour: string) => {
+  const json = await axios({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    url: url + "hourlysales/cat",
+    params: {
+      date,
+      hour
+    }
+  });
+  return json;
+};
