1d4f9d69c79eec665f9667e54e8c5443842dba7b
Stephen <stephenn649@gmail.com>
2025-05-06 10:24:30 -0500

    Integrates hourly slice into the redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-// import slice reducers here
 import appReducer from "../features/appSlice";
 import fieldReducer from "../features/fieldSlice";
 import tableReducer from "../features/tableSlice";
+import hourlyReducer from "../features/hourlySlice";
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     field: fieldReducer,
     table: tableReducer,
+    hourly: hourlyReducer,
   }
 });
 
-// the current state tree is inferred from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// AppDispatch is the type of the dispatch function
 export type AppDispatch = typeof store.dispatch;
