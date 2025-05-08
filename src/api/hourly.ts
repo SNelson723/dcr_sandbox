3a398c9614321a7150e8518f802b7251cf7eaa0d
Stephen <stephenn649@gmail.com>
2025-05-08 14:15:05 -0500

    Sets up the api calls for the dept/subdept items

--- src/api/hourly.ts
+++ src/api/hourly.ts
@@ -1,21 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getTopHourlyDepts = async (url: string, date: string, hour: string) => {
+export const getTopHourlyDepts = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     url: url + "hourlysales/depts",
     params: {
       date,
-      hour
-    }
+      hour,
+    },
   });
   return json;
 };
 
-export const getTopHourlySubDepts = async (url: string, date: string, hour: string) => {
+export const getTopHourlySubDepts = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
     method: "GET",
     headers: {
@@ -24,13 +32,17 @@ export const getTopHourlySubDepts = async (url: string, date: string, hour: stri
     url: url + "hourlysales/subdepts",
     params: {
       date,
-      hour
-    }
+      hour,
+    },
   });
   return json;
 };
 
-export const getTopHourlyCats = async (url: string, date: string, hour: string) => {
+export const getTopHourlyCats = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
     method: "GET",
     headers: {
@@ -39,28 +51,36 @@ export const getTopHourlyCats = async (url: string, date: string, hour: string) 
     url: url + "hourlysales/cat",
     params: {
       date,
-      hour
-    }
+      hour,
+    },
   });
   return json;
 };
 
-export const getTopHourlyTenders = async (url: string, date: string, hour: string) => {
+export const getTopHourlyTenders = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
     method: "GET",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     url: url + "hourlysales/tender",
     params: {
       date,
-      hour
-    }
+      hour,
+    },
   });
   return json;
 };
 
-export const getTopHourlyItems = async (url: string, date: string, hour: string) => {
+export const getTopHourlyItems = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
     method: "GET",
     headers: {
@@ -68,13 +88,18 @@ export const getTopHourlyItems = async (url: string, date: string, hour: string)
     },
     url: url + "hourlysales/toptenitems",
     params: {
-      date, hour
-    }
+      date,
+      hour,
+    },
   });
   return json;
 };
 
-export const getBottomHourlyItems = async (url: string, date: string, hour: string) => {
+export const getBottomHourlyItems = async (
+  url: string,
+  date: string,
+  hour: string
+) => {
   const json = await axios({
     method: "GET",
     headers: {
@@ -83,8 +108,50 @@ export const getBottomHourlyItems = async (url: string, date: string, hour: stri
     url: url + "hourlySales/bottomtenitems",
     params: {
       date,
-      hour
-    }
+      hour,
+    },
+  });
+  return json;
+};
+
+export const getDeptItems = async (
+  url: string,
+  date: string,
+  hour: string,
+  id: string
+) => {
+  const json = await axios({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    url: url + "hourlysales/deptitems",
+    params: {
+      date,
+      hour,
+      id,
+    },
+  });
+  return json;
+};
+
+export const getSubDeptItems = async (
+  url: string,
+  date: string,
+  hour: string,
+  id: string
+) => {
+  const json = await axios({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    url: url + "hourlysales/subdeptitems",
+    params: {
+      date,
+      hour,
+      id,
+    },
   });
   return json;
 };
