c2b5266e18b22b05b4530d8787230d4fb2b5561d
Stephen <stephenn649@gmail.com>
2025-07-24 14:21:20 -0500

    Changes the text color in the Home page for better visibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="mt-12 w-screen flex flex-col items-center justify-center h-[60vh]">
+    <div className="mt-12 w-screen flex flex-col items-center justify-center min-h-[60vh] text-white">
       <div>
         <div className="py-2 px-4 border-b-2 mb-2 border-b-slate-50 text-2xl font-semibold text-center mx-auto">
           Home page
