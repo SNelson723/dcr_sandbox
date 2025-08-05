79f403e4738753d4bdf3a62fa2c1defb62dc475e
Stephen <stephenn649@gmail.com>
2025-08-05 09:53:39 -0500

    Gets rid of the rest of the old nav approach in navMenuData.ts

--- src/data/navMenuData.ts
+++ src/data/navMenuData.ts
@@ -3,18 +3,9 @@ import { UsersIcon } from "@heroicons/react/24/outline";
 import Radiation from "../svgs/Radiation";
 import StoresIcon from "../svgs/StoresIcon";
 
-export type NavMenuItem = {
-  label: string;
-};
-
 export const baseClass =
   "py-2.5 hover:text-white hover:bg-blue-200 hover:text-black font-semibold transition-all duration-300 text-nowrap p-4 cursor-pointer flex items-center justify-start gap-3";
 export const activeClass = "bg-[rgb(174,199,242)] text-white";
-
-export interface NavLinkProps {
-  to: string;
-  label: string;
-}
 
 export type Navigation = {
   name: string;
